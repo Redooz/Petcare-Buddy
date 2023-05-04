2e63d2b14c0dcbeae5f5746befb331ec37f7165b
Nicolás <yohannicolas2003@gmail.com>
2023-05-04 11:22:40 -0500

    feat: Create Signup and Hello xml activities

---
+++ app/src/main/java/com/example/petcarebuddy/views/HelloActivity.kt
@@ -0,0 +1,12 @@
+package com.example.petcarebuddy.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.petcarebuddy.R
+
+class HelloActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello)
+    }
+}
