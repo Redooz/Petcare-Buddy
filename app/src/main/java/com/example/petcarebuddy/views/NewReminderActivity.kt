7765b5ddf38b2de651b722418c5d1bd870c4f6f2
Nicolás <yohannicolas2003@gmail.com>
2023-05-07 11:16:33 -0500

    feat: Create Activity for New Reminder

---
+++ app/src/main/java/com/example/petcarebuddy/views/NewReminderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.petcarebuddy.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.petcarebuddy.R
+
+class NewReminderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_reminder)
+    }
+}
