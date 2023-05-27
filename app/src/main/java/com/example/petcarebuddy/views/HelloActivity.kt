2cce0067474f539fc5a808689d3885c1f90e9a22
Nicolás <yohannicolas2003@gmail.com>
2023-05-26 21:44:55 -0500

    feat: Enable binding

--- app/src/main/java/com/example/petcarebuddy/views/HelloActivity.kt
+++ app/src/main/java/com/example/petcarebuddy/views/HelloActivity.kt
@@ -3,11 +3,14 @@ package com.example.petcarebuddy.views
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.petcarebuddy.R
+import com.example.petcarebuddy.databinding.ActivityHomeBinding
 
 class HelloActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hello)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     companion object {
